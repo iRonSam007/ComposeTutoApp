660a4c5dee09a5700c169cdd905be1bbd8c4003d
iRonSam007 <abida.invest10@gmail.com>
2024-10-26 20:30:48 +0200

    feat: add example of usage, columns, rows, modifiers

--- app/src/main/java/com/example/composetutoapp/MainActivity.kt
+++ app/src/main/java/com/example/composetutoapp/MainActivity.kt
@@ -5,9 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +24,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutoapp.ui.theme.ComposeTutoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,15 +34,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ColRowExample()
+            //DemoColRow()
+            DemoModifiers()
         }
+    }
+}
+
+
+
+
+
+//2 - Example: Using modifiers
+@Composable
+fun DemoModifiers(){
+    Column(
+        modifier = Modifier //Functions of modifier applied sequentielly => Cool stuff with borader and padding: Hell in XML
+            .background(Color.Red)
+            .fillMaxHeight(0.7f)
+            .fillMaxWidth() //.requiredWidth(600.dp)// force a hardcoded width
+            .padding(top = 15.dp)
+            .border(5.dp, Color.Black)
+            .padding(5.dp)
+            .border(5.dp, Color.Blue)
+            .padding(5.dp)
+            .border(5.dp, Color.Yellow)
+
+        //,
+        //horizontalAlignment = Alignment.Start,
+        //verticalArrangement = Arrangement.SpaceAround
+    ){
+        Text("Issam", modifier = Modifier
+            .offset(20.dp, 20.dp))
+        Spacer(modifier = Modifier.height(50.dp)) //Push anything below with a 50dp => Give space to above composable
+
+        Text(" Great", modifier = Modifier
+            .border(5.dp, Color.Black)
+            .padding(5.dp)
+            .border(5.dp, Color.Blue)
+            .padding(5.dp)
+            .border(5.dp, Color.Yellow))
+
+        Text("Why", modifier = Modifier.clickable { //Interactible
+
+        })
     }
 }
 
 
 //1 - Example: Using columns and rows
 @Composable
-fun ColRowExample(){    //NB: Composable function always starts with a capital letter
+fun DemoColRow(){    //NB: Composable function always starts with a capital letter
     Row(
         modifier = Modifier
             .fillMaxSize(0.7f)

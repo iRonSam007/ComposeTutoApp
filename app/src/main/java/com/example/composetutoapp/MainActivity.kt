4826cd38d7632562afb9d516a90e17aa52fc8dc8
iRonSam007 <abida.invest10@gmail.com>
2024-10-26 23:11:59 +0200

    feat: add example of usage of compose image card

--- app/src/main/java/com/example/composetutoapp/MainActivity.kt
+++ app/src/main/java/com/example/composetutoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -18,15 +19,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetutoapp.ui.theme.ComposeTutoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,12 +43,77 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             //DemoColRow()
-            DemoModifiers()
+            //DemoModifiers()
+            imgCardExample()
         }
     }
 }
 
 
+//3 - Example: Using Image Card
+@Composable
+fun imgCardExample(){
+    val painter = painterResource(id = R.drawable.iron_man)
+    val description = "description ... Kermo in snow"
+    val title = "title bla bla"
+    Box(modifier = Modifier
+        .fillMaxSize(0.5f)
+        .padding(10.dp)){
+        ImageCard(painter = painter, contentDescription = description, title = title)
+    }
+}
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        //elevation = 5.dp
+    )
+    {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image( //Image first layer of Card
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box( //Gradient layer applied on the Image
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f //Could be calculated dynamically
+                        )
+                    )
+            )
+            Box( // Top layer of Card containt a text
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Column() {
+                    Text(
+                        title,
+                        style = TextStyle(color = Color.White, fontSize = 20.sp)
+                    )
+                    Text(
+                        contentDescription,
+                        style = TextStyle(color = Color.White, fontSize = 12.sp)
+                    )
+                }
+            }
+        }
+    }
+}
 
 
 
